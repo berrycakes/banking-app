cfdce347cb08b8b3f47edc9cbd71f79786498726
Jianne Gabriel <jiannegabriel@gmail.com>
2022-01-08 10:51:26 +0800

    Fix balance conflict in adding transactions

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,10 @@ onload = () => displayUser()
 const user = sessionStorage.getItem('user')
 const indexedDB = window.indexedDB
 
-const form = document.querySelector('#transaction-form')
+const transactionTypeSelector = document.querySelector('#transaction-type')
+const formTransfer = document.querySelector('#transfer-form')
+const formDeposit = document.querySelector('#deposit-form')
+const formPayment = document.querySelector('#payment-form')
 const submitTransactionBtn = document.querySelector(
   '#submit-add-transaction-btn'
 )
@@ -30,7 +33,93 @@ const displayUser = () => {
   }
 }
 
+const selectTransactionType = () => {
+  if (transactionTypeSelector.value === 'Deposit') {
+    formDeposit.classList.remove('invisible')
+  }
+}
+
 const addTransaction = () => {
+  let recipientAddress = document.querySelector('#recipient').value
+  let amount = parseFloat(document.querySelector('#transfer-amount').value)
+  const request = indexedDB.open('TransactionsDatabase', 1)
+
+  request.onupgradeneeded = () => {
+    const db = request.result
+    const store = db.createObjectStore('transactions', {
+      keypath: 'id',
+      autoIncrement: true,
+    })
+    store.createIndex('type', ['type'], { unique: false })
+    store.createIndex('timestamp', ['timestamp'], { unique: true })
+    store.createIndex('emailAddress', ['emailAddress'], { unique: false })
+    store.createIndex('recipientAddress', ['recipientAddress'], {
+      unique: false,
+    })
+    store.createIndex('amount', ['amount'], { unique: false })
+  }
+  request.onsuccess = () => {
+    const db = request.result
+    const transaction = db.transaction('transactions', 'readwrite')
+    const store = transaction.objectStore('transactions')
+
+    store.put({
+      timestamp: new Date(),
+      emailAddress: user,
+      recipientAddress: recipientAddress,
+      amount: amount,
+    })
+    transaction.oncomplete = () => {
+      console.log('added transaction')
+      db.close()
+    }
+  }
+
+  const requestAccount = indexedDB.open('AccountsDatabase', 1)
+  requestAccount.onsuccess = () => {
+    const db = requestAccount.result
+    const transaction = db.transaction('accounts', 'readwrite')
+    const store = transaction.objectStore('accounts')
+
+    const emailAddress = store.index('emailAddress')
+
+    const senderKey = emailAddress.getKey([user])
+    senderKey.onsuccess = () => {
+      console.log(senderKey.result)
+      const senderQuery = store.get(senderKey.result)
+      senderQuery.onsuccess = () => {
+        const senderData = senderQuery.result
+        senderData.balance -= amount
+        const requestUpdateSender = store.put(senderData, senderKey.result)
+        requestUpdateSender.onsuccess = () => {
+          document.querySelector('#account-balance-container').innerText =
+            senderData.balance
+        }
+      }
+    }
+
+    const recipientKey = emailAddress.getKey([recipientAddress])
+    recipientKey.onsuccess = () => {
+      console.log(recipientKey.result)
+      const recipientQuery = store.get(recipientKey.result)
+      recipientQuery.onsuccess = () => {
+        const recipientData = recipientQuery.result
+        recipientData.balance += amount
+        const requestUpdateRecipient = store.put(
+          recipientData,
+          recipientKey.result
+        )
+        requestUpdateRecipient.onsucess = () => {
+          console.log(recipientData)
+        }
+      }
+    }
+  }
+}
+
+const addIncome = () => {
+  let recipientAddress = document.querySelector('#recipient').value
+  let amount = parseFloat(document.querySelector('#transfer-amount').value)
   const request = indexedDB.open('TransactionsDatabase', 1)
 
   request.onupgradeneeded = () => {
@@ -50,20 +139,62 @@ const addTransaction = () => {
     const db = request.result
     const transaction = db.transaction('transactions', 'readwrite')
     const store = transaction.objectStore('transactions')
-    let recipientAddress = document.querySelector('#recipient').value
-    let amount = document.querySelector('#transfer-amount').value
 
     store.put({
       timestamp: new Date(),
       emailAddress: user,
       recipientAddress: recipientAddress,
-      amount: parseFloat(amount),
+      amount: amount,
     })
-    transaction.oncomplete = () => db.close()
+    transaction.oncomplete = () => {
+      console.log('added transaction')
+      db.close()
+    }
+  }
+
+  const requestAccount = indexedDB.open('AccountsDatabase', 1)
+  requestAccount.onsuccess = () => {
+    const db = requestAccount.result
+    const transaction = db.transaction('accounts', 'readwrite')
+    const store = transaction.objectStore('accounts')
+
+    const emailAddress = store.index('emailAddress')
+
+    const senderKey = emailAddress.getKey([user])
+    senderKey.onsuccess = () => {
+      console.log(senderKey.result)
+      const senderQuery = store.get(senderKey.result)
+      senderQuery.onsuccess = () => {
+        const senderData = senderQuery.result
+        senderData.balance -= amount
+        const requestUpdateSender = store.put(senderData, senderKey.result)
+        requestUpdateSender.onsuccess = () => {
+          document.querySelector('#account-balance-container').innerText =
+            senderData.balance
+        }
+      }
+    }
+
+    const recipientKey = emailAddress.getKey([recipientAddress])
+    recipientKey.onsuccess = () => {
+      console.log(recipientKey.result)
+      const recipientQuery = store.get(recipientKey.result)
+      recipientQuery.onsuccess = () => {
+        const recipientData = recipientQuery.result
+        recipientData.balance += amount
+        const requestUpdateRecipient = store.put(
+          recipientData,
+          recipientKey.result
+        )
+        requestUpdateRecipient.onsucess = () => {
+          console.log(recipientData)
+        }
+      }
+    }
   }
 }
 
-form.addEventListener('submit', (e) => {
+formTransfer.addEventListener('submit', (e) => {
   e.preventDefault()
 })
 submitTransactionBtn.addEventListener('click', addTransaction)
